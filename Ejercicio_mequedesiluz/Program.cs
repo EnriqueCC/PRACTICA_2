897876552f75815fe6fadbe3e119a096c391c556
camargo.enrique@uabc.edu.mx <camargo.enrique@uabc.edu.mx>
2021-08-31 20:11:25 -0700

    nuevo contenido

---
+++ Ejercicio_mequedesiluz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_mequedesiluz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, edad;
+            Console.WriteLine("Escribe tu nombre ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad ");
+            edad = Console.ReadLine();
+            Console.WriteLine(" Tu nombre es  " + nombre + " y tienes " + edad);
+            Console.WriteLine("Preciona cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
